5da827b28a5c95019d246eb9053afd54630b4b93
ST10466873 <st10466873@vcconnect.edu.za>
2026-03-31 22:27:18 +0200

    Add project files.

---
+++ st10466873 PROG6221 part1/Chatbot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace st10466873_PROG6221_part1
+{
+    internal class Chatbot
+    {
+    }
+}
---
+++ st10466873 PROG6221 part1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace st10466873_PROG6221_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
