40cdff5cd0a23897c23966f88902671444785fb7
ST10466873 <st10466873@vcconnect.edu.za>
2026-03-31 22:30:52 +0200

    creating the bot object

--- st10466873 PROG6221 part1/Chatbot.cs
+++ st10466873 PROG6221 part1/Chatbot.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace st10466873_PROG6221_part1
 {
-    internal class Chatbot
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Chatbot bot = new Chatbot();
+            bot.Start();
+        }
     }
 }
