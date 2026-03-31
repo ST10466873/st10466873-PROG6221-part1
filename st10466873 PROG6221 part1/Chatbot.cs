f28c697adf32dac49c5a808ff6f5344e0c62852c
ST10466873 <st10466873@vcconnect.edu.za>
2026-03-31 22:36:09 +0200

    This method displays the logo

--- st10466873 PROG6221 part1/Chatbot.cs
+++ st10466873 PROG6221 part1/Chatbot.cs
@@ -28,3 +28,36 @@ namespace st10466873_PROG6221_part1
                 PrintWithTypingEffect("[System: Audio file 'greeting.wav' not found. Please ensure it is in the correct directory.]\n", ConsoleColor.Red);
             }
         }
+
+        private void DisplayAsciiArt()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(@"
+   _____       _               _____ecurity
+  / ____|     | |             |  _ \       | |
+ | |    _   _ | |__   ___ _ __| |_) | ___  | |_ 
+ | |   | | | || '_ \ / _ \ '__|  _ < / _ \ | __|
+ | |___| |_| || |_) |  __/ |  | |_) | (_) || |_ 
+  \_____\__, ||_.__/ \___|_|  |____/ \___/  \__|
+         __/ |                                  
+        |___/                                   
+            ");
+            Console.ResetColor();
+        }
+
+        private void GreetUser()
+        {
+            PrintWithTypingEffect("--------------------------------------------------", ConsoleColor.DarkGray);
+            PrintWithTypingEffect("Welcome to the Cybersecurity Awareness Bot!", ConsoleColor.Green);
+            PrintWithTypingEffect("--------------------------------------------------", ConsoleColor.DarkGray);
+
+            Console.Write("\nTo get started, please enter your name: ");
+            userName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                userName = "User";
+            }
+
+            PrintWithTypingEffect($"\nHello, {userName}! I'm here to help you stay safe online.", ConsoleColor.Cyan);
+        }
